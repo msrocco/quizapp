ab085a16f08e309f89d382c214532ce7bbd10efa
Matheus Rocco <62848343+msrocco@users.noreply.github.com>
2023-09-12 20:20:37 -0400

    feat: skip question on card pan

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -9,8 +9,10 @@ import Animated, {
   Easing,
   useAnimatedScrollHandler,
   Extrapolate,
+  runOnJS,
 } from "react-native-reanimated";
 import { useNavigation, useRoute } from "@react-navigation/native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 import { styles } from "./styles";
 import { THEME } from "../../styles/theme";
@@ -31,6 +33,9 @@ interface Params {
 
 type QuizProps = (typeof QUIZ)[0];
 
+const CARD_INCLINATION = 10;
+const CARD_SKIP_AREA = -200;
+
 export function Quiz() {
   const [points, setPoints] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
@@ -42,6 +47,7 @@ export function Quiz() {
 
   const shake = useSharedValue(0);
   const scrollY = useSharedValue(0);
+  const cardPosition = useSharedValue(0);
 
   const { navigate } = useNavigation();
 
@@ -163,6 +169,32 @@ export function Quiz() {
     };
   });
 
+  const onPan = Gesture.Pan()
+    .activateAfterLongPress(200)
+    .onUpdate((event) => {
+      const moveToLeft = event.translationX < 0;
+
+      if (moveToLeft) {
+        cardPosition.value = event.translationX;
+      }
+    })
+    .onEnd((event) => {
+      if (event.translationX < CARD_SKIP_AREA) runOnJS(handleSkipConfirm)();
+
+      cardPosition.value = withTiming(0);
+    });
+
+  const dragStyles = useAnimatedStyle(() => {
+    const rotateZ = cardPosition.value / CARD_INCLINATION;
+
+    return {
+      transform: [
+        { translateX: cardPosition.value },
+        { rotateZ: `${rotateZ}deg` },
+      ],
+    };
+  });
+
   useEffect(() => {
     const quizSelected = QUIZ.filter((item) => item.id === id)[0];
     setQuiz(quizSelected);
@@ -204,14 +236,16 @@ export function Quiz() {
           />
         </Animated.View>
 
-        <Animated.View style={shakeStyleAnimation}>
-          <Question
-            key={quiz.questions[currentQuestion].title}
-            question={quiz.questions[currentQuestion]}
-            alternativeSelected={alternativeSelected}
-            setAlternativeSelected={setAlternativeSelected}
-          />
-        </Animated.View>
+        <GestureDetector gesture={onPan}>
+          <Animated.View style={[shakeStyleAnimation, dragStyles]}>
+            <Question
+              key={quiz.questions[currentQuestion].title}
+              question={quiz.questions[currentQuestion]}
+              alternativeSelected={alternativeSelected}
+              setAlternativeSelected={setAlternativeSelected}
+            />
+          </Animated.View>
+        </GestureDetector>
 
         <View style={styles.footer}>
           <OutlineButton title="Stop" onPress={handleStop} />
